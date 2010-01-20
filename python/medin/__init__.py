5a504ab78f9f3ab46db92745576ec9e2bba72ff0
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2010-01-20 16:29:01 +0000

    Split the medin module up into a package
    This enables easier code management and logic encapsulation.

--- python/medin/__init__.py
+++ python/medin/__init__.py
@@ -3,7 +3,6 @@ import os
 
 # Third party modules
 import suds                             # for the SOAP client
-import errata                           # for the error handling
 
 # Helper Functions
 
@@ -121,76 +120,7 @@ class TemplateContext(object):
             tvars = {}
         self.tvars = tvars
 
-# The error handler
-class ErrorHandler(errata.ErrorHandler):
-    """WSGI post-processor for handling errors using Mako templates.
-
-    Exception instances of (or derived from) HTTPError are caught and
-    the appropriate response is returned to the client."""
-
-    def __init__(self, *args, **kwargs):
-        super(ErrorHandler, self).__init__(*args, **kwargs)
-
-        self.add(errata.HTTPError, self.handleHTTPError)
-
-        from mako.exceptions import TopLevelLookupException
-        self.add(TopLevelLookupException, self.handleTemplateLookupException)
-        self.add(Exception, self.handleException)
-
-    def handleHTTPError(self, exception, environ, start_response):
-        """Handler for HTTPErrors"""
-
-        renderer = ErrorRenderer(exception)
-        return renderer(environ, start_response)
-
-    def handleTemplateLookupException(self, exception, environ, start_response):
-        from mako.exceptions import TopLevelLookupException
-        
-        message = 'The template could not be found: %s' % str(exception)
-        e = errata.HTTPError('404 Not Found', message)
-        try:
-            return self.handleHTTPError(e, environ, start_response)
-        except TopLevelLookupException:
-            # The top level template can't be found, so specify a default
-            environ['selector.vars']['template'] = 'light'
-            message = 'The template you specified does not exist.'
-            e = errata.HTTPError('404 Not Found', message)
-            return self.handleHTTPError(e, environ, start_response)
-
-    def handleException(self, exception, environ, start_response):
-        from mako.exceptions import RichTraceback
-        from cStringIO import StringIO
-        
-        traceback = RichTraceback()
-        buf = StringIO()
-        
-        for (filename, lineno, function, line) in traceback.traceback:
-            buf.write("File %s, line %s, in %s\n" % (filename, lineno, function))
-            buf.write(line)
-            buf.write("\n")
-        buf.write("%s: %s\n" % (str(traceback.error.__class__.__name__), traceback.error))
-        output = buf.getvalue()
-        
-        start_response('500 Internal Server Error', [('Content-type', 'text/plain')])
-        return [output]
-
 # The WSGI Applications
-
-class ErrorRenderer(MakoApp):
-    def __init__(self, exception):
-        self.exception = exception
-        super(ErrorRenderer, self).__init__(['%s', 'error.html'])
-
-    def setup(self, environ):
-        title = 'Error - %s' % self.exception.args[0]
-        status = self.exception.args[0]
-        tvars = dict(message=self.exception.args[1])
-        return TemplateContext(title, status=status, tvars=tvars)
-
-class HTTPErrorRenderer(ErrorRenderer):
-    def __init__(self, status, message):
-        e = errata.HTTPError(status, message)
-        super(HTTPErrorRenderer, self).__init__(e)
 
 class OpenSearch(MakoApp):
     def __init__(self):
--- wsgi/search.wsgi
+++ wsgi/search.wsgi
@@ -2,10 +2,10 @@
 import selector                        # for URI based dispatch
 
 # Custom modules
-import medin
+import medin, medin.error
 
 class Selector(selector.Selector):
-    status404 = medin.HTTPErrorRenderer('404 Not Found', 'The resource you specified could not be found')
+    status404 = medin.error.HTTPErrorRenderer('404 Not Found', 'The resource you specified could not be found')
 
 # Create a WSGI application
 application = Selector(consume_path=False)
@@ -26,4 +26,4 @@ application.add('/{template}/catalogue[.{format:word}]', GET=medin.Results())
 application.add('/{template}/catalogue/{gid:digits}', GET=medin.Metadata())
 
 # add our Error handler
-application = medin.ErrorHandler(application)
+application = medin.error.ErrorHandler(application)
