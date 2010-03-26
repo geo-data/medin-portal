c477e83f936f222452bcd15248854efe64d77971
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2010-03-26 16:21:56 +0000

    Added the ability to resize and drag an existing bounding box
    The user can click on an existing bounding box on the spatial search
    map which then highlights the box and disables map navigation using
    the mouse. The user can now drag the box to a new location or resize
    it using the handles in the corners. Altering the size of the box
    updates the search criteria and result count.
    
    Clicking on the box again de-selects the box and re-enables normal
    navigation.

--- html/js/full/search.js
+++ html/js/full/search.js
@@ -1,6 +1,251 @@
 // how long before loading messages should be displayed in milliseconds
 var LOAD_DELAY = 1000;
 
+// jQuery CSS map determining the style of the area box
+var BOX_SELECTED = {
+    'border-color': '#0B8AB3',
+    'opacity': '1',
+    'background-image': "url('/images/area-fill-selected.png')",
+    'background-repeat': 'repeat'
+};
+
+var BOX_DESELECTED = {
+    'border-color': '#CA0000',
+    'opacity': '1',
+    'background-image': "url('/images/area-fill.png')",
+    'background-repeat': 'repeat'
+};
+
+function stop_box_edit(event) {
+    if (!box)
+        return;
+
+    $(box.div).data('edit', false);
+}
+
+function add_box(extent) {
+    // add the box to the map
+    var newbox = new OpenLayers.Marker.Box(extent);
+
+    // move the box based on pixel locations
+    function move_box(minx, miny, maxx, maxy) {
+        var min = map.getLonLatFromPixel(new OpenLayers.Pixel(minx, miny));
+        var max = map.getLonLatFromPixel(new OpenLayers.Pixel(maxx, maxy));
+        newbox.bounds = new OpenLayers.Bounds(min.lon, min.lat, max.lon, max.lat);
+
+        $('#area').val('');                     // unset any predefined area
+        $('#bbox').val(newbox.bounds.toBBOX()); // set the form element
+        check_query();                          // update the query results
+    }
+
+    // add the drag and resize behaviour
+    var div = $(newbox.div);
+    div.css(BOX_DESELECTED)
+    .draggable({
+        containment: '#map',    // don't drag outside of the map
+        disabled: true,
+        cursor: 'crosshair',
+        start: function(event, ui) {
+            $(this).data('edit', false); // prevent box being deselected on stop
+        },
+        stop: function(event, ui) {
+            // replace the existing box bounds with the new one
+            var self = $(this);
+            var minx = ui.position.left;
+            var maxy = ui.position.top;
+            var maxx = minx + self.width();
+            var miny = maxy + self.height();
+
+            move_box(minx, miny, maxx, maxy);
+        }
+    })
+    .resizable({
+        handles: 'all',
+        containment: '#map',    // don't resize outside of the map
+        distance: 5,
+        start: function(event, ui) {
+            $(this).data('edit', false); // prevent box being deselected on stop
+        },
+        stop: function(event, ui) {
+            // replace the existing box bounds with the new one
+            var minx = ui.position.left;
+            var maxy = ui.position.top;
+            var maxx = minx + ui.size.width;
+            var miny = maxy + ui.size.height;
+
+            move_box(minx, miny, maxx, maxy);
+        }
+    }).mousedown(function() {
+        $(this).data('edit', true);
+    }).mouseup(function () {
+        var self = $(this);
+        // Should we check the resizing?
+        if (!self.data('edit'))
+            return;
+
+        if (self.resizable( "option", "disabled" )) {
+            // deactivate all navigation controls
+            var controls = map.getControlsByClass('OpenLayers.Control.Navigation');
+            for (var i = 0; i < controls.length; i++) {
+                controls[i].deactivate()
+            }
+
+            // set the box to be resizable
+            self.resizable( "option", "disabled", false )
+                .draggable( "option", "disabled", false );
+            self.css(BOX_SELECTED);
+        } else {
+            // reactivate all navigation controls
+            var controls = map.getControlsByClass('OpenLayers.Control.Navigation');
+            for (var i = 0; i < controls.length; i++) {
+                controls[i].activate()
+            }
+
+            // disable resizing
+            self.resizable( "option", "disabled", true )
+                .draggable( "option", "disabled", true );
+            self.css(BOX_DESELECTED);
+        }
+    })
+    .resizable( "option", "disabled", true );
+
+    // add the custom handles for the resize
+    div.find('.ui-resizable-ne').addClass('ui-custom-icon ui-icon-gripsmall-diagonal-ne');
+    div.find('.ui-resizable-sw').addClass('ui-custom-icon ui-icon-gripsmall-diagonal-sw');
+    div.find('.ui-resizable-nw').addClass('ui-custom-icon ui-icon-gripsmall-diagonal-nw');
+
+    boxes.addMarker(newbox);
+    
+    clear_box();                // remove any previous box
+
+    bbox = extent;
+    box = newbox;
+    
+    map.zoomToExtent(extent);   // zoom to the box
+
+    $('#bbox').val(extent.toBBOX()); // set the form element
+}
+
+// remove the bounding box and deselect the area
+function clear_area() {
+    var select = $('#area')
+    select.val('');
+    select.change();
+}
+
+function clear_box() {
+    if (box) {
+        boxes.removeMarker(box);
+        box = null;
+    }
+
+    $('#bbox').val('');
+}
+
+function zoom_to_area(id) {
+    get_bbox(id, function(tbbox) {
+        if (!tbbox)
+            return false;
+
+        map.zoomToExtent(OpenLayers.Bounds.fromArray(tbbox));
+        return true;
+    });
+}
+
+// Populate the area selection control
+function populate_areas() {
+    var select = $('#area');
+
+    // ensure a bbox is created when an area is selected
+    select.change(function() {
+        var id = $(this).attr('value');
+        if (!id) {
+            clear_box();
+            return;
+        }
+        
+        get_bbox(id, function(tbbox) {
+            if (!tbbox) {
+                clear_box();
+                return;
+            }
+
+            var extent = new OpenLayers.Bounds(tbbox[0], tbbox[1], tbbox[2], tbbox[3]);
+            add_box(extent);
+        });
+    });
+
+    // if there's an existing area, select it
+    if (area) {
+        select.val(area);
+        select.change();
+    } else if (bbox) {
+        add_box(bbox);
+    } else {
+        // zoom to the UK as a default
+
+        // The map is hidden to begin with and needs to be initialised
+        // when it is first shown
+        $('body').bind('fieldsetview', function(event, id) {
+            if (id != 'spatial-search')
+                return;
+            
+            map.updateSize();
+            zoom_to_area('GB');
+            $(this).unbind(event); // we don't need this event any more
+        });
+    }
+}
+
+function init_date(id) {
+    var input = $('#'+id);
+    var text = $('#'+id+'-text');
+
+    // a function to initialise the date picker
+    var init_datepicker = function() {
+        input.datepicker({
+            onClose: function(new_date, picker) {
+                if (!new_date) {
+                    input.hide();
+                    text.show();
+                }
+            },
+            onSelect: function(date, picker) {
+                // trigger the change event, which is used by the
+                // query checking code.
+                input.change();
+            }
+        });
+    }
+
+    var has_date = input.val();
+    // initialise the date picker if a date is present
+    if (has_date) {
+        init_datepicker();
+        text.hide();
+        input.show();
+    }
+
+    text.one('click', function() {
+        $(this).hide();
+        input.show();
+
+        // initialise the date picker if there was no initial date
+        if (!has_date) {
+            init_datepicker();
+        }
+        
+        input.datepicker('show');
+        
+        // for subsequent clicks
+        text.click(function() {
+            $(this).hide();
+            input.show();
+            input.datepicker('show');
+        });
+    });
+}
+
 function toggle(id, show_txt, hide_txt) {
     var ele = $('#'+id);
     if (ele.is(':visible')) {
