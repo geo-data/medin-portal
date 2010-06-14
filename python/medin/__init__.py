3f07ae8034db55a5488ce30d61044ee08d619d21
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2010-06-14 14:35:23 +0100

    Updated search interface help
    The inline help has been updated to include content from Gaynor,
    including an introduction.

--- python/medin/__init__.py
+++ python/medin/__init__.py
@@ -1,7 +1,7 @@
 # The medin version string. When changes are made to the application
 # this version number should be incremented. It is used in caching to
 # ensure the client gets the latest version of a resource.
-__version__ = 0.96
+__version__ = 0.97
 
 from errata import HTTPError           # for HTTP exceptions
 import medin.error
