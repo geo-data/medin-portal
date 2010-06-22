81755356c7ffbbca18140524bceab55e599115dd
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2010-06-22 19:05:28 +0100

    Licensed code under the Reciprocal Public License 1.5
    The code and associated files have been licensed under the RPL
    1.5. This requires file header information to be provided (as defined
    in the license as EXHIBIT A) which has been added in this commit.
    
    The license is provided at http://opensource.org/licenses/rpl1.5.txt
    
    Third party code retains their original licenses.

--- python/medin/__init__.py
+++ python/medin/__init__.py
@@ -1,3 +1,27 @@
+# Created by Homme Zwaagstra
+# 
+# Copyright (c) 2010 GeoData Institute
+# http://www.geodata.soton.ac.uk
+# geodata@soton.ac.uk
+# 
+# Unless explicitly acquired and licensed from Licensor under another
+# license, the contents of this file are subject to the Reciprocal
+# Public License ("RPL") Version 1.5, or subsequent versions as
+# allowed by the RPL, and You may not copy or use this file in either
+# source code or executable form, except in compliance with the terms
+# and conditions of the RPL.
+# 
+# All software distributed under the RPL is provided strictly on an
+# "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+# INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT, OR
+# NON-INFRINGEMENT. See the RPL for specific language governing rights
+# and limitations under the RPL.
+# 
+# You can obtain a full copy of the RPL from
+# http://opensource.org/licenses/rpl1.5.txt or geodata@soton.ac.uk
+
 # The medin version string. When changes are made to the application
 # this version number should be incremented. It is used in caching to
 # ensure the client gets the latest version of a resource.
