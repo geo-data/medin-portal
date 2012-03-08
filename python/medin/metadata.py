geo-data/medin-portal

commit e1713c3bfd01df16d3b1953f183dfafa4c768cc2

Modify 'keywords' representation
This fixes issue #18.

python/medin/metadata.py
@@ -434,6 +434,7 @@ class Parser(object):
         xpath.xpathRegisterNs('gmd', 'http://www.isotc211.org/2005/gmd')
         xpath.xpathRegisterNs('gco', 'http://www.isotc211.org/2005/gco')
         xpath.xpathRegisterNs('srv', 'http://www.isotc211.org/2005/srv')
+        xpath.xpathRegisterNs('gmx', 'http://www.isotc211.org/2005/gmx')
         xpath.xpathRegisterNs('xlink', 'http://www.w3.org/1999/xlink')
         xpath.xpathRegisterNs('gml', 'http://www.opengis.net/gml/3.2')
 
@@ -634,79 +635,14 @@ class Parser(object):
     def keywords(self):
         """Element 11: Keywords"""
         
-        def keywordListFromTitle(title):
-            """
-            Parse a keyword title and return the list key if present
-
-            e.g. the title "SeaDataNet BODC Vocabulary (P011)" returns
-            P011. A case insensitive match is also done on the title for
-            the string 'inspire', in which case the code P220 is
-            returned.
-            """
-
-            if 'inspire' in title.lower():
-                return 'P220'
-
-            match = self._keyword_pattern.search(title)
-            if not match:
-                return None
-
-            return match.groups()[0]
-
-        from terms import VocabError
-        
-        keywords = {}
+        keywords = []
         for node in self.xpath.xpathEval('//gmd:descriptiveKeywords/gmd:MD_Keywords'):
             self.xpath.setContextNode(node)
             # try and retrieve the keywords
             try:
-                words = [word.content.strip() for word in self.xpath.xpathEval('./gmd:keyword/gco:CharacterString/text()')]
+                keywords.extend([word.content.strip() for word in self.xpath.xpathEval('./gmd:keyword/gco:CharacterString/text() | ./gmd:keyword/gmx:Anchor/text()')])
             except IndexError:
                 continue
-
-            # try and get a code and a title for the list to which the
-            # keywords belong
-            try:
-                code = self.xpath.xpathEval('.//gmd:MD_KeywordTypeCode/@codeListValue')[0].content.strip()
-                title = code
-            except IndexError:
-                try:
-                    title = self.xpath.xpathEval('.//gmd:title/gco:CharacterString/text()')[0].content.strip()
-                except IndexError:
-                    code = 'unknown'
-                    title = code
-                else:
-                    code = keywordListFromTitle(title)
-
-            for word in words:
-                # try and get a definition for the term from the list code
-                if code:
-                    try:
-                        defn = self.vocab.lookupTerm(code, word)
-                    except LookupError:
-                        defn = {'short':word,
-                                'long':word,
-                                'defn':'Unknown term'}
-                    except VocabError, e:
-                        defn = {'error': e.message}
-                else:
-                    defn = {}
-
-                # the Discovery Web Service indexes PO21 parameters by
-                # long name, not short, so we get rid of the short
-                # name...
-                if code == 'P021':
-                    try:
-                        defn['short'] = defn['long']
-                    except KeyError:
-                        pass
-
-                # add the definition to the keyword dictionary
-                try:
-                    keywords[title][word] = defn
-                except KeyError:
-                    defns = {word: defn}
-                keywords[title] = defns
 
         return keywords
 

templates/full/metadata.html
@@ -144,6 +144,9 @@ ${date.date().isoformat()}\
     % endif
 </%def>\
 
+<%def name="format_url_content(x)" buffered="True">${x | q,u,x}</%def>\
+<%def name="format_xml_content(x)" buffered="True">${x | x}</%def>\
+
 % if metadata.bbox:
 <!-- Element 12 -->
 <div class="extent right">
@@ -187,40 +190,6 @@ ${date.date().isoformat()}\
 
 % if metadata.parent_id:
 <p><strong>Parent ID:</strong> ${metadata.parent_id}</p>
-% endif\
-
-% if metadata.keywords:
-<!-- Element 11 -->
-<p class="no-bottom"><strong>Keywords:</strong></p>
-  % for title, defns in metadata.keywords.items():
-  <div><strong>${title | x}</strong></div>
-  <div class="subitem">
-    % for i, (keyword, defn) in enumerate(defns.items()):
-      % if i:
-      <div class="sibling">
-      % else:
-      <div>
-      % endif
-      % if 'error' in defn:
-        <span class="error">${defn['error']}</span>
-      % else:
-        <% if defn:
-             if defn['short']: keyword = defn['short']
-             else: keyword = defn['long']
-        %>
-        <a href="${script_root}/full/catalogue?q=p:${keyword | q,u,x}"
-           title="Search for all metadata having the parameter '${keyword | x}'">${keyword | x}</a>
-        % if defn:
-          % if defn['short'] and defn['long'] != defn['short']:
-          - ${defn['long'] | x}
-          % endif
-        <br/>Definition: ${defn['defn'] | x}
-        % endif
-      % endif
-      </div>
-    % endfor
-  </div>
-  % endfor
 % endif\
 
 <h2>Details</h2>
@@ -387,6 +356,33 @@ ${resource['link']}\
   </tr>
   % endif\
 
+  % if metadata.keywords:
+  <!-- Element 11 -->
+  <tr>
+    <td class="heading">
+      <div class="cell">
+        <strong>Keywords</strong> (<a href="javascript:toggle('element-11', 'info', 'hide info')" id="element-11-link" title="Show or hide the element information">info</a>)
+      </div>
+    </td>
+    <td class="content">
+      <div id="element-11" class="cell element-info">
+        <p>Keywords belonging to various MEDIN approved thesauri.</p>
+      </div>
+      <div class="cell">
+        <%self:output_element element="${metadata.keywords}" number="${11}">
+  <% 
+     keywords = []
+     for keyword in metadata.keywords:
+       xml_keyword = format_xml_content(keyword)
+       keywords.append('<a href="' + script_root + '/full/catalogue?q=p:' + format_url_content(keyword) + '" title="Search for all metadata having the parameter `' + xml_keyword + '`">' + xml_keyword + '</a>')
+  %>
+  ${', '.join(keywords)}
+        </%self:output_element>
+      </div>
+    </td>
+  </tr>
+  % endif\
+
   % if metadata.bbox:
   <!-- Element 12 -->
   <tr>
@@ -425,7 +421,7 @@ ${resource['link']}\
     </td>
   </tr>
   % endif\
-  
+
   % if metadata.access_limits:
   <!-- Element 20 -->
   <tr>

templates/light/metadata.html
@@ -108,6 +108,9 @@ ${date.strftime('%Y-%m-%d')}\
   % endif
 </%def>\
 
+<%def name="format_url_content(x)" buffered="True">${x | q,u,x}</%def>\
+<%def name="format_xml_content(x)" buffered="True">${x | x}</%def>\
+
 <!-- Element 3 -->
 <h2>Abstract</h2>
 <p>${metadata.abstract}</p>
@@ -148,39 +151,6 @@ ${date.strftime('%Y-%m-%d')}\
   Download the extent in <a href="${resource_root}/kml" title="View the extent as a KML file"><img alt="KML" width="16" height="16" src="/images/kml.png" class="icon"/> KML format</a>.
 </p>
 % endif
-
-% if metadata.keywords:
-<!-- Element 11 -->
-<h2>Keywords</h2>
-  % for title, defns in metadata.keywords.items():
-  <h3>${title | x}</h3>
-  <dl>
-    % for keyword, defn in defns.items():
-    <dt>
-      % if 'error' in defn:
-        <span class="error">${defn['error']}</span>
-    </dt>
-      % else:
-        <% if defn:
-             if defn['short']: keyword = defn['short']
-             else: keyword = defn['long']
-        %>
-        <a href="${script_root}/light/catalogue?q=p:${keyword | q,u,x}"
-           title="Search for all metadata having the parameter '${keyword | x}'">${keyword | x}</a>
-        % if defn:
-          % if defn['short'] and defn['long'] != defn['short']:
-          - ${defn['long'] | x}
-          % endif
-    </dt>
-    <dd>${defn['defn'] | x}</dd>
-        % else:
-    </dt>
-        % endif
-      % endif
-    % endfor
-  </dl>
-  % endfor
-% endif\
 
 <h2>Metadata Details</h2>
 
@@ -238,6 +208,20 @@ ${resource['link']}\
     % endif
   % endfor
   </dl>
+</%self:output_element>
+% endif\
+
+% if metadata.keywords:
+<!-- Element 11 -->
+<h3>Keywords</h3>
+<%self:output_element element="${metadata.keywords}">
+<% 
+   keywords = []
+   for keyword in metadata.keywords:
+     xml_keyword = format_xml_content(keyword)
+     keywords.append('<a href="' + script_root + '/full/catalogue?q=p:' + format_url_content(keyword) + '" title="Search for all metadata having the parameter `' + xml_keyword + '`">' + xml_keyword + '</a>')
+%>
+<p>${', '.join(keywords)}</p>
 </%self:output_element>
 % endif\
 

commit a0acac258bc0cfdaee463a0d190dc6e30e95d0a0

Fix the metadata CSV download
This was originally broken by commit
e1713c3bfd01df16d3b1953f183dfafa4c768cc2 for issue #18.

python/medin/metadata.py
@@ -1301,16 +1301,7 @@ def metadata2csv(metadata, file):
 
     writer.writerows(iter_element_values(10, 'Spatial data service type', metadata.service_type))
 
-    row = metadata.keywords
-    if row and not isinstance(row, Exception):
-        tmp = []
-        for title, defns in row.items():
-            for keyword, defn in defns.items():
-                entry = [title]
-                entry.extend(vocab2row(defn, [keyword]))
-                tmp.append(entry)
-        row = tmp
-    writer.writerows(iter_element_values(11, 'Keywords', row))
+    writer.writerows(iter_element_values(11, 'Keywords', metadata.keywords))
 
     row = metadata.bbox
     if row and not isinstance(row, Exception):