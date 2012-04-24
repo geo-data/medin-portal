geo-data/medin-portal

commit aaf4543e77d967c361ff5b728fbb9d20af8955ce

Add NERC DDS ‘marithesaurus’ function
This was adapted and improved from the code submitted by Olly from the
NERC DDS. It fixes issue #16.

python/medin/query.py
@@ -88,6 +88,9 @@ class Query(GETParams):
         except KeyError:
             pass
 
+    def clone(self):
+        return Query(str(self), self.areas, self.fields, self.max_count)
+
     def verify(self):
         """
         Check the validity of the Query, logging any errors
@@ -160,6 +163,22 @@ class Query(GETParams):
             if self.raise_errors: raise
 
         return default
+
+    def addSearchTerm(self, term):
+        try:
+            self['q'][0] += ' %s' % term
+        except KeyError, AttributeError:
+            self['q'] = [term]
+
+    def replaceSearchTerm(self, find, replace):
+        import re
+        pattern = re.compile(r'^("%s"|%s)$' % (find, find), re.IGNORECASE)
+        terms = []
+        for op_or, op_not, target, word in self.getSearchTerm(skip_errors=True):
+            term = (op_or, op_not, target, pattern.sub(replace, word))
+            terms.append(' '.join((t for t in term if t)))
+        if terms:
+            self['q'][0] = ' '.join(terms)
 
     def getStartDate(self, cast=True, default=''):
         return self.asDate('sd', cast, default, True)

python/medin/views.py
@@ -606,8 +606,10 @@ class Results(MakoApp):
 class HTMLResults(Results):
     def __init__(self):
         from medin.dws import RESULT_BRIEF
+        from medin.terms import Vocabulary
 
         super(HTMLResults, self).__init__(['%s', 'catalogue.html'], RESULT_BRIEF)
+        self.vocab = Vocabulary()
 
     def setup(self, environ):
         from copy import deepcopy
@@ -615,6 +617,17 @@ class HTMLResults(Results):
         # create the data structure for the template sort logic
         ctxt = super(HTMLResults, self).setup(environ)
         query = deepcopy(ctxt.tvars['query'])
+
+        # set up the related terms mapping
+        mapping = {}
+        for op_or, op_not, target, word in query.getSearchTerm(skip_errors=True):
+            related = self.vocab.getRelated('P211', word.strip('"'))
+            if not related:
+                continue
+            mapping[word] = related
+        ctxt.tvars['mapping'] = mapping
+
+        # set up the sort criteria
         ctxt.tvars['criteria'] = query.asDict(False)
 
         sorts = {}

python/medin/terms.py
@@ -90,11 +90,12 @@ class Vocabulary(object):
             try:
                 term = r.verifiedTerm
             except AttributeError:
-                raise LookupError('The term "%s" does not exist in the list %s' % (term, listKey))
+                raise LookupError('The term \'%s\' does not exist in the list %s' % (term, listKey))
 
         return dict(long=term.entryTerm,
                     short=term.entryTermAbbr,
-                    defn=term.entryTermDef)
+                    defn=term.entryTermDef,
+                    key=term.entryKey)
 
     def getList(self, list_id):
         """
@@ -109,6 +110,34 @@ class Vocabulary(object):
                          defn=entry.entryTermDef) for entry in r[0]]
         except TypeError:
             return []
+
+    def getRelated(self, list_id, term):
+        """Get related terms"""
+        related = {}
+        def getRelationship(relationship, record):
+            try:
+                related[relationship] = [term.entryTerm for term in record['%sMatch' % relationship]]
+            except AttributeError:
+                pass
+
+        try:
+            term = self.lookupTerm(list_id, term)
+        except LookupError:
+            return related
+
+        recordKey = [term['key']]
+        objectKey = []
+        predicate = 255
+        inference = 'true'
+        r = self._callService(self.client.service.getRelatedRecordByTerm, recordKey, predicate, objectKey, inference)
+
+        record = r.codeTableRecord[0]
+        getRelationship('minor', record)
+        getRelationship('exact', record)
+        getRelationship('broad', record)
+        getRelationship('narrow', record)
+
+        return related
 
 class CachedVocabulary(Vocabulary):
     """
@@ -286,7 +315,7 @@ class StaticVocabList(object):
                         short=entry['abbr'],
                         defn=entry['defn'])
         except KeyError:
-            raise LookupError('The term "%s" does not exist in the list' % term)
+            raise LookupError('The term \'%s\' does not exist in the list' % term)
         except AttributeError:
             pass
 
@@ -301,3 +330,4 @@ class StaticVocabList(object):
     def getList(self):
         from copy import deepcopy
         return deepcopy(self.vocab)
+

templates/full/catalogue.html
@@ -92,19 +92,23 @@ ${query_string | x}\
             height: 400px;
             border: 1px solid #ADACA8;
         }
-        #geographic-coverage {
+        .fieldset {
             padding: 0.5em;
-            margin-bottom: 0.75em;
+            margin-bottom: 1em;
             border: 1px solid #ADACA8;
         }
-        #geographic-coverage.off {
+        .fieldset dd {
+            margin-left: 1em;
+            margin-bottom: 0.5em;
+        }
+        div.off {
             border-bottom: 0;
             border-left: 0;
             border-right: 0;
             padding-bottom: 0;
-            margin-bottom: 0;
+            margin-bottom: 0.5;
         }
-        #geographic-coverage h4 {
+        #geographic-coverage h4, #related-searches h4 {
             font-size:1.4em; 
             font-weight:normal;
             margin-top: -1em;
@@ -233,6 +237,47 @@ $(function() {
 </div>
 </%def>
 
+<%def name="related_searches()">
+% if mapping:
+<div id="related-searches" class="fieldset">
+  <h4 class="box"><a href="javascript:toggle_fieldset('related-searches')" title="Show or hide related searches">Related searches</a></h4>
+  <div class="content">
+  <p>For the text term(s) you entered alternatives, sourced from the Marisaurus thesaurus, are available below.  The use of an alternative term may influence the records returned. To substitute your term with an alternative, click on its link to resubmit your search.  This process may be repeated as required.  Note: your other search criteria will be preserved.
+  </p>
+  %for term, related in mapping.items():
+      <%
+         relationships = {
+           'minor': 'Minor matches for',
+           'exact': 'Synonyms for',
+           'broad': 'Broader terms for',
+           'narrow': 'Narrower terms for'
+         }
+      %>
+      <dl>
+      % for relationship, mapped in related.items():
+          <dt>${relationships[relationship]} <kbd>${term}</kbd></dt>
+          <dd>
+          <%
+             cmap = []
+             for map in mapped:
+               q = query.clone()
+               q.replaceSearchTerm(term, '"%s"' % map)
+               cmap.append((map, str(q)))
+          %>
+          %for i, (mapped, q) in enumerate(cmap):
+            %if i:
+,
+            %endif
+            <a href="${script_root}/full/catalogue?${str(q)}">${mapped | x}</a>\
+          %endfor
+          </dd>
+      % endfor
+      </dl>
+  % endfor
+  </div>
+</div>
+% endif
+</%def>
 
 %if hits == 0:
 <p>Your search did not return any results. Try <a href="${script_root}/full${query_string | x}" title="Edit the current search criteria">editing your search</a> 
@@ -242,6 +287,7 @@ to correct the errors.
 to make it less restrictive.
 %endif
 </p>
+${related_searches()}
 <%return '' %>
 %endif
 
@@ -272,9 +318,10 @@ ${start_index} - ${end_index}
 % else:
 all results
 % endif
-</strong>:</p>
-
-<div id="geographic-coverage" class="off">
+</strong>:
+</p>
+${related_searches()}
+<div id="geographic-coverage" class="fieldset off">
   <h4 class="box"><a href="javascript:toggle_fieldset('geographic-coverage')" title="Show or hide the geographic coverage of the results">Geographic coverage</a></h4>
   <div class="content" style="display:none">
     <p>The geographic coverage of the results on this page. Note that this does <strong>not</strong> include datasets with global coverage.</p>

templates/light/catalogue.html
@@ -102,6 +102,44 @@ ${query_string | x}\
 % endif
 </%def>
 
+<%def name="related_searches()">
+% if mapping:
+  <h3>Related searches</h3>
+  <p>For the text term(s) you entered alternatives, sourced from the Marisaurus thesaurus, are available below.  The use of an alternative term may influence the records returned. To substitute your term with an alternative, click on its link to resubmit your search.  This process may be repeated as required.  Note: your other search criteria will be preserved.
+  </p>
+  %for term, related in mapping.items():
+      <%
+         relationships = {
+           'minor': 'Minor matches for',
+           'exact': 'Synonyms for',
+           'broad': 'Broader terms for',
+           'narrow': 'Narrower terms for'
+         }
+      %>
+      <dl>
+      % for relationship, mapped in related.items():
+          <dt>${relationships[relationship]} <kbd>${term}</kbd></dt>
+          <dd>
+          <%
+             cmap = []
+             for map in mapped:
+               q = query.clone()
+               q.replaceSearchTerm(term, '"%s"' % map)
+               cmap.append((map, str(q)))
+          %>
+          %for i, (mapped, q) in enumerate(cmap):
+            %if i:
+,
+            %endif
+            <a href="${script_root}/light/catalogue?${str(q)}">${mapped | x}</a>\
+          %endfor
+          </dd>
+      % endfor
+      </dl>
+  % endfor
+% endif
+</%def>
+
 %if hits == 0:
 <p>Your search did not return any results. Try <a href="${script_root}/light${query_string | x}" title="Edit the current search criteria">editing your search</a> 
 %if log:
@@ -110,6 +148,7 @@ to correct the errors.
 to make it less restrictive.
 %endif
 </p>
+${related_searches()}
 <%return '' %>
 %endif
 
@@ -136,6 +175,7 @@ all results\
 % endif
 </strong>:</p>
 
+${related_searches()}
 ${result_links()}
 
 % for i, res in enumerate(results):

commit 3cf756d293761784cd395e40190d389754838c71

Fixed a bug in the serialisation of query strings
This bug was introduced in commit
aaf4543e77d967c361ff5b728fbb9d20af8955ce which resulted in query
strings with targets not being serialised properly.

python/medin/dws.py
@@ -87,17 +87,19 @@ class TermBuilder(object):
 
         # Create the termSearch objects from the tokens
         terms = []
-        for i, (op_or, op_not, target, word) in enumerate(tokens):
+        for i, token in enumerate(tokens):
             op = ''
             if i:
-                if op_or and op_not:
+                if token.or_ and token.not_:
                     op = 'OR_NOT'
-                elif op_not:
+                elif token.not_:
                     op ='AND_NOT'
-                elif op_or:
+                elif token.or_:
                     op = 'OR'
-            elif op_not:
+            elif token.not_:
                 op = 'NOT'
+
+            word = token.word
 
             # If the term is a phrase ensure it is enclosed with two
             # pairs of quotes for the DWS
@@ -107,10 +109,10 @@ class TermBuilder(object):
             term = self.client.factory.create('ns0:SearchCriteria.TermSearch')
             term.Term = word
             try:
-                term.TermTarget = self.targets[target.lower()]
+                term.TermTarget = self.targets[token.target.lower()]
             except KeyError:
                 if not skip_errors:
-                    raise ValueError('The following target is not recognised: %s' % target)
+                    raise ValueError('The following target is not recognised: %s' % token.target)
                 term.TermTarget = self.targets['']
 
             term._id = i+1

python/medin/query.py
@@ -174,9 +174,9 @@ class Query(GETParams):
         import re
         pattern = re.compile(r'^("%s"|%s)$' % (find, find), re.IGNORECASE)
         terms = []
-        for op_or, op_not, target, word in self.getSearchTerm(skip_errors=True):
-            term = (op_or, op_not, target, pattern.sub(replace, word))
-            terms.append(' '.join((t for t in term if t)))
+        for token in self.getSearchTerm(skip_errors=True):
+            token.word = pattern.sub(replace, token.word)
+            terms.append(str(token))
         if terms:
             self['q'][0] = ' '.join(terms)
 
@@ -410,6 +410,33 @@ class TargetError(QueryError):
     """
     pass
 
+class TermToken(object):
+    """
+    A combination of a word, target and query operators
+
+    Multiple TermTokens can be combined to define a query. Query
+    operators are `OR` and `-` (NOT).
+    """
+
+    def __init__(self, word, target=None, not_=False, or_=False):
+        self.word = word
+        self.target = target
+        self.not_ = not_
+        self.or_ = or_
+
+    def __str__(self):
+        ret = ''
+        if self.or_:
+            ret += 'OR '
+        if self.not_:
+            ret += '-'
+        if self.target:
+            ret += '%s:' % self.target
+        return ret + self.word
+
+    def __repr__(self):
+        return "TermToken('%s')" % str(self)
+
 class TermParser(object):
     """
     Parse a search term into a list of tokens
@@ -446,7 +473,8 @@ class TermParser(object):
                 continue        # skip reserved words
             if target not in self.targets:
                 bad_targets.append(target)
-            tokens.append((op_or, op_not, target, word))
+            token = TermToken(word, target, bool(op_not), bool(op_or))
+            tokens.append(token)
 
         if not skip_errors and bad_targets:
             targets = [t for t in self.targets if t]
@@ -476,22 +504,22 @@ class TermAnalyser(object):
         Returns query tokens transformed into a human friendly structure
         """
 
-        op_map = {'': 'and',
-                  'OR': 'or'}
+        op_map = {False: 'and',
+                  True: 'or'}
         query = []
-        for i, (op_or, op_not, target, word) in enumerate(tokens):
-            op = op_map[op_or]
+        for i, token in enumerate(tokens):
+            op = op_map[token.or_]
             ops = []
             if i and op:
                 ops.append(op)
-            if op_not:
+            if token.not_:
                 ops.append('not')
 
             try:
-                targets = [target, self.mapping[target]]
+                targets = [token.target, self.mapping[token.target]]
             except KeyError:
-                if target:
-                    targets = [target, None]
+                if token.target:
+                    targets = [token.target, None]
                 else:
                     targets = [None, None]
 
@@ -502,7 +530,7 @@ class TermAnalyser(object):
                 
             term = {'op': op,
                     'target': targets,
-                    'word': word}
+                    'word': token.word}
             query.append(term)
 
         return query

python/medin/views.py
@@ -620,11 +620,11 @@ class HTMLResults(Results):
 
         # set up the related terms mapping
         mapping = {}
-        for op_or, op_not, target, word in query.getSearchTerm(skip_errors=True):
-            related = self.vocab.getRelated('P211', word.strip('"'))
+        for token in query.getSearchTerm(skip_errors=True):
+            related = self.vocab.getRelated('P211', token.word.strip('"'))
             if not related:
                 continue
-            mapping[word] = related
+            mapping[token.word] = related
         ctxt.tvars['mapping'] = mapping
 
         # set up the sort criteria