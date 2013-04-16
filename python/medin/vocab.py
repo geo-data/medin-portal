fe99c877e830975718a6343253b25ef6e6ebd248
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2013-04-16 16:45:21 +0100

    Fix association of parameters with data themes and sub themes
    Parameters are now associated with data themes and sub themes using
    *both* `narrower` and `related` concepts rather than just the former
    as was previously the case.

--- python/medin/vocab.py
+++ python/medin/vocab.py
@@ -1,16 +1,16 @@
 # Created by Homme Zwaagstra
-# 
+#
 # Copyright (c) 2012 GeoData Institute
 # http://www.geodata.soton.ac.uk
 # geodata@soton.ac.uk
-# 
+#
 # Unless explicitly acquired and licensed from Licensor under another
 # license, the contents of this file are subject to the Reciprocal
 # Public License ("RPL") Version 1.5, or subsequent versions as
 # allowed by the RPL, and You may not copy or use this file in either
 # source code or executable form, except in compliance with the terms
 # and conditions of the RPL.
-# 
+#
 # All software distributed under the RPL is provided strictly on an
 # "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR
 # IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,7 +18,7 @@
 # FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT, OR
 # NON-INFRINGEMENT. See the RPL for specific language governing rights
 # and limitations under the RPL.
-# 
+#
 # You can obtain a full copy of the RPL from
 # http://opensource.org/licenses/rpl1.5.txt or geodata@soton.ac.uk
 
@@ -61,9 +61,10 @@ class Vocabularies(Mapping):
             .filter(skos.Collection.uri == collection)\
             .filter(skos.Concept.prefLabel.ilike(term)).first()
 
-    def getConceptsHavingBroader(self, source_collection, broader_member):
+    def getSubConcepts(self, source_collection, associated_member):
         """
-        Retrieve all concepts from a collection having a particular broader member
+        Retrieve all concepts from a collection having a particular broader or
+        related member
         """
 
         sql = """SELECT o.*
@@ -76,26 +77,36 @@ AND c1.uri = c2c1.concept_uri
 AND c2c1.collection_uri = ?
 AND c1.uri = cb1.narrower_uri
 AND cb1.broader_uri = ?
-ORDER BY c1.prefLabel"""
+UNION
+SELECT o.*
+FROM object o,
+     concept c1,
+     concepts2collections c2c1,
+     concept_related cr1
+WHERE o.uri = c1.uri
+AND c1.uri = c2c1.concept_uri
+AND c2c1.collection_uri = ?
+AND (c1.uri = cr1.left_uri OR c1.uri = cr1.right_uri)
+AND (cr1.right_uri = ? OR cr1.left_uri = ?)"""
         conn = self.session.connection()
-        result = conn.execute(sql, [source_collection, broader_member])
+        result = conn.execute(sql, [source_collection, associated_member, source_collection, associated_member, associated_member])
         for concept in self.session.query(skos.Concept).instances(result):
             yield concept
 
     def getSubThemeIdsForDataThemeId(self, data_theme_id):
         broader = 'http://vocab.nerc.ac.uk/collection/P23/current/' + data_theme_id
-        return self.getIdsFromConcepts(self.getConceptsHavingBroader('http://vocab.nerc.ac.uk/collection/P03/current', broader))
+        return self.getIdsFromConcepts(self.getSubConcepts('http://vocab.nerc.ac.uk/collection/P03/current', broader))
 
     def getParameterIdsForSubThemeId(self, sub_theme_id):
         broader = 'http://vocab.nerc.ac.uk/collection/P03/current/' + sub_theme_id
-        return self.getIdsFromConcepts(self.getConceptsHavingBroader('http://vocab.nerc.ac.uk/collection/P02/current', broader))
-            
+        return self.getIdsFromConcepts(self.getSubConcepts('http://vocab.nerc.ac.uk/collection/P02/current', broader))
+
     def getDataThemeIds(self):
         return self.getMemberIdsFromCollection('http://vocab.nerc.ac.uk/collection/P23/current')
 
     def getDataFormatIds(self):
         return self.getIdsFromConcepts(self['http://vocab.nerc.ac.uk/collection/M01/current'].members.values())
-    
+
     def getDataFormatsFromIds(self, ids):
         return self.getConceptsFromIds(ids, 'http://vocab.nerc.ac.uk/collection/M01/current')
 
@@ -104,7 +115,7 @@ ORDER BY c1.prefLabel"""
 
     def getAccessTypesFromIds(self, ids):
         return self.getConceptsFromIds(ids, 'medin-access-types.xml', '%s')
-    
+
     def getMemberIdsFromCollection(self, uri):
         concepts = self[uri].members.values()
         concepts.sort(cmp=lambda a, b: cmp(a.prefLabel, b.prefLabel))
@@ -119,7 +130,7 @@ ORDER BY c1.prefLabel"""
             .join(skos.Concept.collections)\
             .filter(skos.Collection.uri == collection_uri)\
             .filter(filt).all()
-        
+
     def getDataThemesFromIds(self, ids):
         return self.getConceptsFromIds(ids, 'http://vocab.nerc.ac.uk/collection/P23/current')
 
@@ -128,64 +139,89 @@ ORDER BY c1.prefLabel"""
 
     def getParametersFromIds(self, ids):
         return self.getConceptsFromIds(ids, 'http://vocab.nerc.ac.uk/collection/P02/current')
-    
+
     def getIdsFromConcepts(self, concepts):
-        return [(c.uri.rsplit('/', 1)[-1], c.prefLabel) for c in concepts]
+        return sorted([(c.uri.rsplit('/', 1)[-1], c.prefLabel) for c in concepts], cmp=lambda a, b: cmp(a[1], b[1]))
 
     def getParametersFromDataThemeIds(self, ids):
         if not ids:
             return []
 
+        placeholder = ','.join('?' * len(ids))
+
+        sub_themes = """SELECT c1.uri
+      FROM concept c1,
+           concepts2collections c2c1,
+           concept_broader cb1
+      WHERE c1.uri = c2c1.concept_uri
+      AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P03/current'
+      AND c1.uri = cb1.narrower_uri
+      AND cb1.broader_uri IN (%s)
+
+      UNION
+
+      SELECT c1.uri
+      FROM concept c1,
+           concepts2collections c2c1,
+           concept_related cr1
+      WHERE c1.uri = c2c1.concept_uri
+      AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P03/current'
+      AND (c1.uri = cr1.left_uri OR c1.uri = cr1.right_uri)
+      AND (cr1.right_uri IN (%s) OR cr1.left_uri IN (%s))""" % (placeholder, placeholder, placeholder)
+
         sql = """SELECT c1.prefLabel
 FROM concept c1,
      concepts2collections c2c1,
-     concept c2,
-     concepts2collections c2c2,
      concept_broader cb1,
-     concept c3,
-     concepts2collections c2c3,
-     concept_broader cb2
+     (%s) AS sub_themes
+
 WHERE c1.uri = c2c1.concept_uri
 AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P02/current'
-
-AND c2.uri = c2c2.concept_uri
-AND c2c2.collection_uri = 'http://vocab.nerc.ac.uk/collection/P03/current'
-
 AND c1.uri = cb1.narrower_uri
-AND c2.uri = cb1.broader_uri
+AND cb1.broader_uri = sub_themes.uri
 
-AND c3.uri = c2c3.concept_uri
-AND c2c3.collection_uri = 'http://vocab.nerc.ac.uk/collection/P23/current'
+UNION
 
-AND c2.uri = cb2.narrower_uri
-AND c3.uri = cb2.broader_uri
+SELECT c1.prefLabel
+FROM concept c1,
+     concepts2collections c2c1,
+     concept_related cr1,
+     (%s) AS sub_themes
 
-AND c3.uri IN (%s)""" % ','.join('?' * len(ids))
+WHERE c1.uri = c2c1.concept_uri
+AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P02/current'
+AND (c1.uri = cr1.left_uri OR c1.uri = cr1.right_uri)
+AND (cr1.right_uri = sub_themes.uri OR cr1.left_uri = sub_themes.uri)""" % (sub_themes, sub_themes)
 
         conn = self.session.connection()
         params = ['http://vocab.nerc.ac.uk/collection/P23/current/%s' % id_ for id_ in ids]
-        return [row[0] for row in conn.execute(sql, params)]
+        return [row[0] for row in conn.execute(sql, params * 6)]
 
     def getParametersFromSubThemeIds(self, ids):
         if not ids:
             return []
 
+        placeholders = ','.join('?' * len(ids))
         sql = """SELECT c1.prefLabel
 FROM concept c1,
      concepts2collections c2c1,
-     concept c2,
-     concepts2collections c2c2,
      concept_broader cb1
 WHERE c1.uri = c2c1.concept_uri
 AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P02/current'
-
-AND c2.uri = c2c2.concept_uri
-AND c2c2.collection_uri = 'http://vocab.nerc.ac.uk/collection/P03/current'
-
 AND c1.uri = cb1.narrower_uri
-AND c2.uri = cb1.broader_uri
+AND cb1.broader_uri IN (%s)
 
-AND c2.uri IN (%s)""" % ','.join('?' * len(ids))
+UNION
+
+SELECT c1.prefLabel
+FROM concept c1,
+     concepts2collections c2c1,
+     concept_related cr1
+WHERE c1.uri = c2c1.concept_uri
+AND c2c1.collection_uri = 'http://vocab.nerc.ac.uk/collection/P02/current'
+AND (c1.uri = cr1.left_uri OR c1.uri = cr1.right_uri)
+AND (cr1.right_uri IN (%s) OR cr1.left_uri IN (%s));
+""" % (placeholders, placeholders, placeholders)
         conn = self.session.connection()
         params = ['http://vocab.nerc.ac.uk/collection/P03/current/%s' % id_ for id_ in ids]
-        return [row[0] for row in conn.execute(sql, params)]
+        return [row[0] for row in conn.execute(sql, params * 3)]
